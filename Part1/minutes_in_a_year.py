5ce062f8a32712c12486561f356e71f77ce4df2b
glkink <99361502+alright212@users.noreply.github.com>
2022-07-23 18:51:06 +0300

    Add files via upload

---
+++ Part1/minutes_in_a_year.py
@@ -0,0 +1,4 @@
+# Write your solution here
+print("Minutes in a year:")
+# 365 days a year and 24 hours each day
+print(365*(24*60))
---
+++ Part1/name_and_address.py
@@ -0,0 +1,8 @@
+# Write your solution here
+name = input("Given name: ")
+familyName = input("Family name: ")
+address = input("Street address: ")
+code = input("City and postal code: ")
+print(name + " "+ familyName)
+print(address)
+print(code)
