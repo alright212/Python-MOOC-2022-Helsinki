6fcdb686bed85a5286f44c2ddbc160401765e6aa
glkink <99361502+alright212@users.noreply.github.com>
2022-07-25 23:57:12 +0300

    Add files via upload

---
+++ Part2/alphabetically_in_the_middle.py
@@ -0,0 +1,16 @@
+letter1 = input("1st letter: ")
+letter2 = input("2nd letter: ")
+letter3 = input("3rd letter: ")
+
+if letter1 < letter2 < letter3:
+    print("The letter in the middle is", letter2)
+elif letter1 < letter3 < letter2:
+    print("The letter in the middle is", letter3)
+elif letter2 < letter1 < letter3:
+    print("The letter in the middle is", letter1)
+elif letter2 < letter3 < letter1:
+    print("The letter in the middle is", letter3)
+elif letter3 < letter1 < letter2:
+    print("The letter in the middle is", letter1)
+elif letter3 < letter2 < letter1:
+    print("The letter in the middle is", letter2)
